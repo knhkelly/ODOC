dc5abbaf9524ed8f21e195fee957b5c6cea5afc3
nhDidi <knhkelly@gmail.com>
2023-12-13 20:41:19 +0900

    [200~[PCCE 기출문제] 9번 / 이웃한 칸~(미해결)

---
+++ src/odoc_231213/Solution.java
@@ -0,0 +1,24 @@
+package odoc_231213;
+
+class Solution {
+    public int solution(String[][] board, int h, int w) {
+        int answer = 0;
+        
+        int count = 0;
+        int[] dh = {-1, 0, 1, 0};
+        int[] dw = {0, 1, 0, -1};
+        int checkH = 0;
+        int checkW = 0;
+            for(int i=0; i<4; i++){
+                try{
+                    checkH = h + dh[i];
+                    checkW = w + dw[i];
+                    if(board[h][w] == board[checkH][checkW]){count++;}
+                } catch(Exception e){continue;}
+                finally {answer = count;}
+            }
+            
+        
+        return answer;
+    }
+}
