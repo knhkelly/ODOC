30aa48784f46e2604d2e013f32627351660405a4
nhDidi <knhkelly@gmail.com>
2023-12-18 15:26:56 +0900

    [PCCE 기출문제] 1번 ~ 7번

---
+++ src/odoc_231218/Solution.java
@@ -0,0 +1,13 @@
+package odoc_231218;
+
+public class Solution {
+    public static void main(String[] args) {
+        String msg = "Spting is beginning";
+        int val1 = 3;
+        String val2 = "3";
+
+        System.out.println(msg);
+        System.out.println(val1 + 10);
+        System.out.println(val2 + "10");
+    }
+}
---
+++ src/odoc_231218/Solution2.java
@@ -0,0 +1,15 @@
+package odoc_231218;
+
+import java.util.Scanner;
+
+public class Solution2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int c = sc.nextInt();
+
+        int b_square = c*c - a*a;
+
+        System.out.println(b_square);
+    }
+}
