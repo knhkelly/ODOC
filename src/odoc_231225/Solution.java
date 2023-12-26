c0caf73f87261aa0a420cabc4de9a0135bae289a
nhDidi <knhkelly@gmail.com>
2023-12-26 12:06:40 +0900

    level 1. 둘만의 암호(해결)

--- src/odoc_231225/Solution.java
+++ src/odoc_231225/Solution.java
@@ -5,51 +5,44 @@ import java.util.*;
 public class Solution {
 	public String solution(String s, String skip, int index) {
 		
-		int[] sNum = new int[s.length()];
-		int[] answerNum = new int[s.length()];
-		for(int i=0; i<s.length(); i++) {
-			sNum[i] = (int)s.charAt(i); 
-			answerNum[i] = (int)s.charAt(i) + index;
-//			if(answerNum[i]>122) { 
-//				answerNum[i] -= 26;
-//			}
-		}
-		
-		List<Integer> skipNum = new ArrayList<Integer>();
-		for(int i=0; i<skip.length(); i++) {
-			int charInt = (int)skip.charAt(i);
-			if(!skipNum.contains(charInt))
-			skipNum.add(charInt);
-		}
-		
-		for(int i=0; i<sNum.length; i++) {
-			for(int j: skipNum) {
-				System.out.println("----------------");
-				System.out.println("sNum["+i+"] : " + sNum[i] + " = " +(char)sNum[i]);
-				System.out.println("lastNum["+i+"] : " + answerNum[i] + " = " + (char)answerNum[i]);
-				System.out.println("skipNum : " + j + " = " + (char)j);
-//				if(j>=sNum[i] && j<=answerNum[i]) {
-//					answerNum[i]++;
-//				} else if(sNum[i]<=answerNum[i]) {
-//					answerNum[i]++;
-//				}
-				if(answerNum[i]<=122) {
-					if(j>=sNum[i] && j<=answerNum[i]) answerNum[i]++;
-				} else if(answerNum[i]>122 && answerNum[i]<148) {
-					
-				} else {
-					
-				}
-				System.out.println("----------------");
-			}
-		}
-		
 		String answer = "";
-		for(int i=0; i<answerNum.length; i++) {
-			while(answerNum[i]>122) {answerNum[i] -= 26;}
-			answer = answer + (char)answerNum[i];
-		}
 		
+        List<String> allphabet = new ArrayList<String>();
+        for(int i=97; i<123; i++) {
+        	char alphabet = (char)i;
+        	allphabet.add(String.valueOf(alphabet));
+        }
+        
+        List<String> skipList = new ArrayList<String>();
+        for(int i=0; i<skip.length(); i++) {
+        	skipList.add(String.valueOf(skip.charAt(i)));
+        }
+        
+        System.out.println("allphabet : " + allphabet);
+        System.out.println("skipList : " + skipList);
+        
+        for(int i=0; i<s.length(); i++){
+        	String searchAlphabet = String.valueOf(s.charAt(i));
+        	int num = allphabet.indexOf(searchAlphabet);
+        	System.out.println("searchAlphabet : " + searchAlphabet + ", num : " + num);
+//        	String num = String.valueOf(s.charAt(i));
+        	
+        	int count = 0;
+        	for(int j=1; count<=index; j++) {
+        		int countNum = num+j;
+        		if(countNum>=26) {while(countNum>=26)countNum -= 26;}
+        		String dd = allphabet.get(countNum);
+        		System.out.println(dd);
+        		if(!skipList.contains(allphabet.get(countNum))) {
+        			count++;
+        			System.out.println("count : " + count);
+        			if(count==index)answer += allphabet.get(countNum);
+        		}
+        		System.out.println("answer : " + answer);
+        	}
+        }
+        
+        
         return answer;
     }
 }
