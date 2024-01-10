b33a11fd7a4f236a9ffa26cec986b07ac74a8581
knhkelly <knhkelly@gmail.com>
2024-01-10 19:53:11 +0900

    level 2.연속 부분 수열 합의 개수(미해결)

--- src/odoc_240109/Solution.py
+++ src/odoc_240109/Solution.py
@@ -1,18 +1,25 @@
-def sum(elements):
-    elLeng = len(elements)
-    if elLeng == 0:
-        return 0
-    elif elLeng == 1:
-        return elements[0]
-    return sum(elements[:len(elements)-1]) + elements[-1]
-
 def solution(elements):
     sumList = []
+    slice = []
+    index = 0
     for i in range(len(elements)):
-        slice = elements[0:i]
-        sumList.append(sum(slice))
+        for count in range(len(elements)):
+            slice.append(elements[index])
+            print("slice")
+            print(slice)
+            index += 1
+            # index 값을 추가하지 않는 경우가 있다! 이걸 어떻게 계산하지?
+            if(index >= len(elements)):
+                index = 0
+            if len(slice) == i+1:
+                sumList.append(sum(slice))
+                slice.clear()
+
+
+    print("sumList")
     print(sumList)
-    answer = 0
+    answer = len(sumList)
     return answer
 
-solution([7,9,1,1,4])
+result = solution([7,9,1,1,4])
+print(result)
