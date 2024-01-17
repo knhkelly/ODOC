937cb0252dc7a9ff5804e6df994c0b09cd859057
knhkelly <knhkelly@gmail.com>
2024-01-17 14:43:48 +0900

    level 1.달리기 경주(시간초과)

--- src/odoc_240116/Solution.java
+++ src/odoc_240116/Solution.java
@@ -4,22 +4,19 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[] players, String[] callings) {
+    	
+    	//초기 답안
     	List<String> runnings = Arrays.asList(players);
-//    	for(int i=0; i<callings.length; i++) {
-//    		int nowIndex = running.indexOf(callings[i]);
-//    		Collections.swap(running, nowIndex, nowIndex-1);
-//    	}
-    	
-    	Map<String, Integer> running = new HashMap<>();
-    	for(int i=0; i<players.length; i++) {
-    		running.put(players[i],i);
-    	}
     	for(int i=0; i<callings.length; i++) {
-    		int nowIndex = running.get(callings[i]);
-    		running.put(callings[i], nowIndex-1);
+    		int nowIndex = runnings.indexOf(callings[i]);
+    		Collections.swap(runnings, nowIndex, nowIndex-1);
     	}
-    	
-        String[] answer = runnings.toArray(new String[running.size()]);
+        
+        String[] answer = runnings.toArray(new String[runnings.size()]);
         return answer;
+    	
+     
+    	
+    	
     }
 }
