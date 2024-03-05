d9b4828d29bfe6256b7ca75f3dc8648dfa238289
knhkelly <knhkelly@gmai.com>
2024-03-05 19:45:43 +0900

    level 2.주차 요금 계산(진행중)

--- src/odoc_240119/Solution.java
+++ src/odoc_240119/Solution.java
@@ -4,7 +4,49 @@ import java.util.*;
 
 public class Solution {
 	public int[] solution(int []arr) {
+//        List<Integer> delArr = new ArrayList<Integer>();
+//        for(int i=0; i<arr.length; i++) {
+//        	delArr.add(arr[i]);
+//        }
 //        
+//        int index=1;
+//        while(index<delArr.size()) {
+//        	if(delArr.get(index)==delArr.get(index-1)) {
+//        		delArr.remove(index);
+//        	}
+//        	else index++;
+//        }
+//        
+//        int[] answer = new int[delArr.size()];
+//        for(int i=0; i<delArr.size(); i++) {
+//        	answer[i] = delArr.get(i).intValue();
+//        }
+		
+		
+//		// 배열 arr를 HashMap delArr로 변환
+//		Map<Integer, Integer> origArr = new HashMap<Integer, Integer>();
+//		for(int i=0; i<arr.length; i++) {
+//			origArr.put(i, arr[i]);
+//		}
+//		
+//		// delArr에서 앞과 중복되는 값 삭제
+//		int index = 0;
+//		for(int i=1;i<arr.length; i++) {
+//			if(origArr.get(i)==origArr.get(index)) {
+//				origArr.remove(i);
+//			} else {
+//				index = i;
+//			}
+//		}
+//		
+//		Set<Integer> keySet = origArr.keySet();
+//		index=0;
+//		int[] answer = new int[origArr.size()];
+//		for(Integer key:keySet) {
+//			answer[index] = origArr.get(key);
+//			index++;
+//        }
+		
 		Stack<Integer> stack = new Stack<Integer>();
 		int arrLength = arr.length;
 		for(int i=0; i<arrLength; i++) {
--- src/odoc_240304/Solution.java
+++ src/odoc_240304/Solution.java
@@ -2,52 +2,105 @@ package odoc_240304;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.List;
 
 public class Solution {
 	public int[] solution(int[] fees, String[] records) {
-        int[] answer = {};
-        
         List<String[]> list = new ArrayList<String[]>();
         for(String record : records) {
         	String[] oneRecord = record.split(" ");
         	list.add(oneRecord);
         }
         
-        //뒤에서부터 차례대로 차번호가 같은 기록을 모아서(리스트에 넣는다?) 주차시간 계산 -> 계산이 끝난 기록은 리스트에서 삭제
-        //리스트 개수가 홀수(마지막 out기록이 없음)일 때와 짝수일 때를 비교해서 계산
-        //리스트가 빌 때까지 반복
-        int count = list.size();
-        String carTime = list.get(0)[0];
-    	String carNum = list.get(0)[1];
-    	String inout = list.get(0)[2];
-    	List<String[]> carData = new ArrayList<String[]>();
-        for(int i=count-1; i>=0; i--) {
-        	if(list.get(i)[1].equals(carNum)) {
-        		carData.add(list.get(i));
-        		list.remove(i);
-        	}
-//        	if(i==count-1 && inout.equals("IN")) {
-//        		String lastTime = "23:59";
-//        		//String을 date자료형으로 형변환
-//        		Date time1 = sdf.parse(carTime);
-//        		Date time2 = sdf.parse(lastTime);
-//        		//date자료형을 밀리세컨즈로
-//        		long timeMil1 = time1.getTime();
-//        		long timeMil2 = time2.getTime();
-//        		//마지막 입차 계산(분단위)
-//        		long diff = (timeMil1 - timeMil2) / (1000*60);
-//        		timeFee += diff;
-//        	}
-        	
+        List<int[]> carList = new ArrayList<int[]>();
+        while(list.size()>0) {
+        	carList.add(feeCal(list));
         }
         
-//        try{
-//	        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
-//    	}catch (ParseException e){
-//			e.printStackTrace();
-//		}
+        Collections.sort(carList, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] - o2[0];
+            }
+        });
+        
+        int[] answer = new int[carList.size()];
+        for(int i=0; i<carList.size(); i++) {
+        	answer[i] = carList.get(i)[1];
+        }
         
         return answer;
     }
+	
+	public int[] feeCal (List<String[]> list) {
+		int count = list.size();
+		String carNum = list.get(0)[1];
+		
+		List<String[]> carData = new ArrayList<String[]>();
+		for(int i=count-1; i>=0; i--) {
+			if(list.get(i)[1].equals(carNum)) {
+				carData.add(list.get(i));
+				list.remove(i);
+			}
+		}
+		
+//        for(int i=0; i<list.size(); i++) {
+//        	System.out.println(list.get(i)[0] + ", " + list.get(i)[1] + ", " + list.get(i)[2]);
+//        }
+//        System.out.println("========================");
+//        for(int i=0; i<carData.size(); i++) {
+//        	System.out.println(carData.get(i)[0] + ", " + carData.get(i)[1] + ", " + carData.get(i)[2]);
+//        }
+
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+		int parkTime = 0; 
+		for(int i=0; i<carData.size(); i+=2) {
+			String in;
+			String out;
+			if(carData.size()%2==1 && i==carData.size()-1) {
+				in = carData.get(i)[0];
+				out = "23:59";
+			} else {
+				in = carData.get(i)[0];
+				out = carData.get(i+1)[0];
+			}
+			try {
+				//String을 date자료형으로 형변환
+				Date time1 = sdf.parse(in);
+				Date time2 = sdf.parse(out);
+				//date자료형을 밀리세컨즈로
+				long timeMil1 = time1.getTime();
+				long timeMil2 = time2.getTime();
+				//입차 계산(분단위) //왜 여기서 0000번만 빼고 시간이 마이너스 대로 나오지?
+				long diff = (timeMil2 - timeMil1) / (1000*60);
+				parkTime += diff;
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		System.out.println("carNum" + carNum);
+		System.out.println(parkTime);
+		
+		int fee = 0;
+		if(parkTime<=180) {
+			fee = 5000;
+		} else {
+			fee = 5000 + (parkTime-180)/10*600;
+		}
+		
+		int carNumInt = Integer.parseInt(carNum);
+		int[] carFee = {carNumInt, fee};
+		System.out.println(carFee[1]);
+		
+		return carFee;
+	}
+        
+        
+       
 }
