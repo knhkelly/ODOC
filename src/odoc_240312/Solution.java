e3a04aae9ccdb9c3f5092d6d20d7fbbaa89f86be
knhkelly <knhkelly@gmai.com>
2024-03-18 18:24:41 +0900

    level 2.삼각달팽이(해결)

--- src/odoc_240312/Solution.java
+++ src/odoc_240312/Solution.java
@@ -14,57 +14,32 @@ public class Solution {
 		List<int[]> triangle = new ArrayList<int[]>();
 		for(int i=1; i<=n; i++) {
 			int[] line = new int[i];
-			if(i==1) line[0]=1;
 			triangle.add(line);
 		}
 		
-		//삼각형의 가장 겉테두리 번호를 매기는 식(이거 하드코딩인가?)
-		//이걸 내부 삼각형에 적용하려면 어떻게 해야하지?
-//		int last = n+(n-1)+(n-2);
-//       	for(int i=1; i<=n+(n-1); i++) {
-//        	triangle.get(i-1)[0] = i;
-//        	if(i>1 && i<n) {
-//        		triangle.get(i-1)[i-1] = last;
-//        		last--;
-//        	}else if(i==n){
-//        		int count = 0;
-//        		for(i=n; i<=n+(n-1); i++) {
-//        			triangle.get(n-1)[count] = i;
-//        			count++;
-//        		}
-//        	}
-//        }
+		int count = 1;
+		int start = 0;
+		int end = n-1;
+		while(count <= allNum) {
+			count = pyramid(start, end, count, triangle);
+			start += 2;
+			end--;
+//			for(int i=0; i<triangle.size(); i++) {
+//	        	System.out.print("{");
+//	        	for(int j=0; j<triangle.get(i).length; j++) {
+//	        		System.out.print(triangle.get(i)[j] + " ");
+//	        	}
+//	        	System.out.println("}");
+//	        }
+		}
 		
-		int last = n+(n-1)+(n-2);
-		int roop = 0;
-		int line = 1;
-       	for(int i=2; i<=n+(n-1)+(n-2)+(n-3); i++) {
-       		int index = triangle.get(roop).length/2;
-       		triangle.get(line)[index] = i;
-       			
-        	if(i>1 && i<n) {
-        		triangle.get(i-1)[i-1] = last;
-        		last--;
-        	}else if(i==n){
-        		int count = 0;
-        		for(i=n; i<=n+(n-1); i++) {
-        			triangle.get(n-1)[count] = i;
-        			count++;
-        		}
-        		i = n+(n-1)+(n-2);
-        		line = roop+1;
-        		roop+=2;
-        	}
-        	line += 1;
-        }
-        
-        for(int i=0; i<triangle.size(); i++) {
-        	System.out.print("{");
-        	for(int j=0; j<triangle.get(i).length; j++) {
-        		System.out.print(triangle.get(i)[j] + " ");
-        	}
-        	System.out.println("}");
-        }
+		count = 0;
+		for(int i=0; i<triangle.size(); i++) {
+			for(int j=0; j<triangle.get(i).length; j++) {
+				answer[count] = triangle.get(i)[j];
+				count++;
+			}
+		}
         
         return answer;
     }
@@ -72,5 +47,38 @@ public class Solution {
 	public int allNum(int n) {
 		if(n==1) return 1;
 		return allNum(n-1)+n;
+	}
+	
+	public int pyramid(int start, int end, int count, List<int[]> triangle){
+		int point = triangle.get(start).length/2;
+		for(int i=start; i<=end; i++) {
+			triangle.get(i)[point] = count;
+			count++;
+		}
+		
+		if(end==triangle.size()-1) {
+			int last = triangle.get(end).length-1;
+			for(int i=point+1; i<=last; i++) {
+				triangle.get(end)[i] = count;
+				count++;
+				if(i==last) point = i;
+			}
+		} else {
+			while(triangle.get(end)[point+1]==0) {
+				triangle.get(end)[point+1] = count;
+				count++;
+				point++;
+			}
+		}
+		
+		int line = end-1;
+		while(line>start) {
+			triangle.get(line)[point-1] = count;
+			count++;
+			line--;
+			point--;
+		}
+		
+		return count;
 	}
 }
